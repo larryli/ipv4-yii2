c7b13649cc388b871006c8b357ed6cd97140c7f9
Larry Li <larryli@qq.com>
2016-05-09 10:04:39 +0800

    fix migrate no yii2 2.0.8 & providers bug
    Signed-off-by: Larry Li <larryli@qq.com>

--- migrations/m150909_153358_ipv4_index.php
+++ migrations/m150909_153358_ipv4_index.php
@@ -14,19 +14,23 @@ class m150909_153358_ipv4_index extends Migration
     public function up()
     {
         $tableOptions = null;
+        $idType = $this->bigPrimaryKey();
         if ($this->db->driverName === 'mysql') {
             // http://stackoverflow.com/questions/766809/whats-the-difference-between-utf8-general-ci-and-utf8-unicode-ci
             $tableOptions = 'CHARACTER SET utf8 COLLATE utf8_unicode_ci ENGINE=InnoDB';
+        } else if ($this->db->driverName === 'sqlite') {
+            // https://github.com/yiisoft/yii2/commit/af75262541e83f353a36baf888b3392c9d34f201#commitcomment-17395513
+            $idType = $this->primaryKey();
         }
         /**
          * @var $ipv4 \larryli\ipv4\yii2\IPv4
          */
         $ipv4 = Yii::$app->get('ipv4');
         foreach ($ipv4->providers as $name => $provider) {
-            if (!empty($provider)) {
+            if (!empty($provider['providers'])) {
                 $table = $this->tableName($name);
                 $this->createTable($table, [
-                    'id' => $this->bigPrimaryKey(),
+                    'id' => $idType,
                     'division_id' => $this->integer(),
                 ], $tableOptions);
             }
@@ -43,7 +47,7 @@ class m150909_153358_ipv4_index extends Migration
          */
         $ipv4 = Yii::$app->get('ipv4');
         foreach ($ipv4->providers as $name => $provider) {
-            if (!empty($provider)) {
+            if (!empty($provider['providers'])) {
                 $this->dropTable($this->tableName($name));
             }
         }
